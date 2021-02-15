131709d79a87d58aa9239415e0ddee995198159e
Maricris <crisremluters@gmail.com>
2021-02-15 21:51:32 +0800

    message

---
+++ prelim_exer1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<title> 
+<?php
+&var ="PHP Tutorial";
+echo "&var";
+?>
+</title>
+
+<body>
+
+<?php
+echo 'PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language.
+ It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.';
+?>
+<a href="mylink.php"> Go to my link </a>
+</body>
+</html>
