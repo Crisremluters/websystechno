af2b5b9630380a4cfa473056004de1ff98c889a5
Maricris <crisremluters@gmail.com>
2021-03-22 20:42:31 +0800

    new message

---
+++ Mid_Act.php
@@ -0,0 +1,25 @@
+<!Doctype Html>
+<html>
+<?php include("Class.php"); ?>
+
+<head>
+<body>
+
+<?Php
+
+
+$ferrari = new car("Ferrari 488 GTB");
+$ferrari->set_maxspeed("205 mph");
+echo "ferrari's maxspeed is: "$ferrari->get_maxspeed();
+echo " I'm a sportscar";
+<br>
+$lexus = lexus new car ();
+$lexus->set_car("lexus");
+echo "Beep!  I am a lexus: " . $lexus->get_car();
+echo "I am an ordinary car"
+
+?>
+</body>
+</html>
+
+
